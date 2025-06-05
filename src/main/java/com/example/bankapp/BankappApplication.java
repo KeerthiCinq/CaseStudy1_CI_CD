6499094f931f0a47de7f0cb597f54f22dcf4dc6e
KeerthiCinq <102806339+KeerthiCinq@users.noreply.github.com>
2025-06-05 15:29:54 +0530

    Update BankappApplication.java

--- src/main/java/com/example/bankapp/BankappApplication.java
+++ src/main/java/com/example/bankapp/BankappApplication.java
@@ -1,13 +1,21 @@
-package com.example.bankapp;
+package com.javatechie.cicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class BankappApplication {
+@RestController
+public class GithubCicdActionsApplication {
+
+	@GetMapping("/welcome")
+	public String welcome(){
+		return "Spring boot app deployment in Kubernetes cluster !";
+	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(BankappApplication.class, args);
+		SpringApplication.run(GithubCicdActionsApplication.class, args);
 	}
 
 }
